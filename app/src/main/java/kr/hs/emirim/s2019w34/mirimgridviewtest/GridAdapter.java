2439168880a6e7302188925944ceb540cf36d515
LeeSeungA <seunga4170@gmail.com>
2020-12-07 19:52:37 +0900

    complete second GridViewTest

--- app/src/main/java/kr/hs/emirim/s2019w34/mirimgridviewtest/GridAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019w34/mirimgridviewtest/GridAdapter.java
@@ -14,7 +14,9 @@ public class GridAdapter extends BaseAdapter {
             R.drawable.poster06,R.drawable.poster07,R.drawable.poster08,R.drawable.poster09,R.drawable.poster10,
             R.drawable.poster11,R.drawable.poster12,R.drawable.poster13,R.drawable.poster14,R.drawable.poster15,
             R.drawable.poster16, R.drawable.poster01, R.drawable.poster02,R.drawable.poster03,R.drawable.poster04};
-
+    String[] posterName = {"극한직업", "백두산", "어벤져스", "엑시트", "완득이", "황해",
+            "너의 이름은", "히말라야", "국제시장", "토이스토리4", "겨울왕국", "업",
+            "몬스터 주식회사", "하울의 움직이는 성", "스폰지밥", "스머프"};
     public GridAdapter(Context context){
         this.context = context;
     }
@@ -49,8 +51,10 @@ public class GridAdapter extends BaseAdapter {
                 AlertDialog.Builder dlg = new AlertDialog.Builder(context);
                 ImageView imgvPoster = dialogView.findViewById(R.id.imgv_dialog);
                 imgvPoster.setImageResource(imgRes[pos]);
-                dlg.setTitle("큰 포스터");
-                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setTitle(posterName[pos]);
+//                dlg.setTitle("큰 포스터");
+//                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setIcon(R.drawable.film);
                 dlg.setView(dialogView);
                 dlg.setNegativeButton("닫기", null);
                 dlg.show();
